17d50fca1ac5be2bf2d2ba3cfa3ce109f94a988a
tonan <tonantruong@hotmail.com>
2023-06-30 22:41:01 -0400

    react-hooks-practice-sushi-saga

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -4,7 +4,7 @@ import MoreButton from "./MoreButton";
 function SushiContainer(props) {
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
+      {/* Render Sushi components here!!! */}
       <MoreButton />
     </div>
   );
